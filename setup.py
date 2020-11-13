fe4688c5e51e996fad97010929f88d737bc9a9ea
Allen Goodman <allen.goodman@icloud.com>
2020-11-13 14:27:49 -0500

    added notebook dep

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ NOTEBOOKS = glob.glob(os.path.join("notebooks", "*.ipynb"))
 
 setuptools.setup(
     install_requires=[
+        "notebook>=6.1.5",
         "numpy>=1.19.4",
         "pandas>=1.1.4",
         "plotnine>=0.7.1",
         "pycytominer@https://github.com/cytomining/pycytominer/archive/master.zip",
-        "requests>=2.25.0",
+        "requests>=2.25.0"
     ],
     name="cytominer-pipeline-examples",
     package_data={
