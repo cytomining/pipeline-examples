736515d2de88940c2e39d54cf51b0bd6c211201b
Allen Goodman <allen.goodman@icloud.com>
2020-11-13 14:21:18 -0500

    packaging

---
+++ setup.py
@@ -0,0 +1,26 @@
+import glob
+import os.path
+
+import setuptools
+
+NOTEBOOKS = glob.glob(os.path.join("notebooks", "*.ipynb"))
+
+setuptools.setup(
+    install_requires=[
+        "numpy>=1.19.4",
+        "pandas>=1.1.4",
+        "plotnine>=0.7.1",
+        "pycytominer@https://github.com/cytomining/pycytominer/archive/master.zip",
+        "requests>=2.25.0",
+    ],
+    name="cytominer-pipeline-examples",
+    package_data={
+        "cytominer_pipeline_examples": NOTEBOOKS
+    },
+    package_dir={
+        "": "src"
+    },
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8, <4",
+    version="1.0.0"
+)
