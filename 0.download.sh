59d59e420e898c00e33c1e91ffa8856d46cb5a20
Shantanu Singh <shsingh@broadinstitute.org>
2022-04-14 10:34:57 -0400

    Update 0.download.sh

--- 0.download.sh
+++ 0.download.sh
@@ -1,3 +1,3 @@
 # Download a publicly available plate of Cell Painting data processed with CellProfiler
 
-aws s3 cp s3://imaging-platform/projects/2018_05_30_ResistanceMechanisms_Kapoor/workspace/backend/2020_07_02_Batch8/218360/218360.sqlite data/218360.sqlite
+aws s3 cp --no-sign-request s3://imaging-platform/projects/2018_05_30_ResistanceMechanisms_Kapoor/workspace/backend/2020_07_02_Batch8/218360/218360.sqlite data/218360.sqlite
